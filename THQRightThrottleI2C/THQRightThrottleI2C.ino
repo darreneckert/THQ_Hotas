e881c3294a3d952d5e7b6643bd3012b32723f8b4
Darren Eckert <darren.e.eckert@gmail.com>
2026-01-16 18:30:12 +1100

    Fixing issues with I2C code, still more work to be done.

--- THQRightThrottleI2C/THQRightThrottleI2C.ino
+++ THQRightThrottleI2C/THQRightThrottleI2C.ino
@@ -2,38 +2,38 @@
 #include <Joystick.h>
 
 #define DEBUGTHQ TRUE
-#define HID_ID    0x26     // Unique HID Device ID, randomly chosen
+#define HID_ID    0x26          // Unique HID Device ID, randomly chosen
 
 // Slew sensor definitions
-#define SLEW_ADR  0x41     // I2C Addrewss of the warthog slew sensor
-#define CTR_REG1  0x0F     // Control Register 1
-#define CTR_REGT  0x2D     // Timing Control Register
-#define CTR_REG2  0x2E     // Control Register 2
-#define ID_CODE   0x0C     // 
-#define X_REG     0x10     // X Register
-#define Y_REG     0x11     // Y Register
-#define XP_REG    0x12     // Positive X Reg
-#define XN_REG    0x13     // Negative X Reg
-#define YP_REG    0x14     // Positive Y Reg
-#define YN_REG    0x15     // Negative Y Reg
+volatile byte SLEW_ADR = 0x41;  // I2C Addrewss of the warthog slew sensor
+volatile byte CTR_REG1 = 0x0F;  // Control Register 1
+volatile byte CTR_REGT = 0x2D;  // Timing Control Register
+volatile byte CTR_REG2 = 0x2E;  // Control Register 2
+volatile byte ID_CODE  = 0x0C;  // 
+volatile byte X_REG    = 0x10;  // X Register
+volatile byte Y_REG    = 0x11;  // Y Register
+volatile byte XP_REG   = 0x12;  // Positive X Reg
+volatile byte XN_REG   = 0x13;  // Negative X Reg
+volatile byte YP_REG   = 0x14;  // Positive Y Reg
+volatile byte YN_REG   = 0x15;  // Negative Y Reg
 
-const int uBtns    = 9;     // Number of buttons on the device
-const int hBtns    = 4;     // Hat switches (U, D, L, R)
-const int dTime    = 50;    // Delay time between loops
-int i;                      // Generic counter variable
+const int uBtns    = 9;         // Number of buttons on the device
+const int hBtns    = 4;         // Hat switches (U, D, L, R)
+const int dTime    = 100;       // Delay time between loops
+int i;                          // Generic counter variable
 
-int xSlew = 0;              // Slew X-Axis value;
-int ySlew = 0;              // Slew Y-Axis value;
-int sBtn_CurrState;         // Current state of the button
-int sHat_CurrState;         // or hat direction being checked
-int sBtn_LastState[uBtns];  // The last recorded state of each button
-int sHat_LastState[hBtns];  // and hat directions
-int sBtn[uBtns] = {         // Digital pins assigned to each button
-  4,                        // Slew push
-  5, 6,                     // Speedbrake forward and back
-  7, 8, 9,                  // Comm 1, Comm 2 and push
-  10, 16,                   // Boat forward and back
-  A2                        // Hat push
+int xSlew = 0;                  // Slew X-Axis value;
+int ySlew = 0;                  // Slew Y-Axis value;
+int sBtn_CurrState;             // Current state of the button
+int sHat_CurrState;             // or hat direction being checked
+int sBtn_LastState[uBtns];      // The last recorded state of each button
+int sHat_LastState[hBtns];      // and hat directions
+int sBtn[uBtns] = {             // Digital pins assigned to each button
+  4,                            // Slew push
+  5, 6,                         // Speedbrake forward and back
+  7, 8, 9,                      // Comm 1, Comm 2 and push
+  10, 16,                       // Boat forward and back
+  A2                            // Hat push
 };
 int sHat[hBtns] = {14, 15, A0, A1};       // Digital pins for the 5 way hat. Up, down, left, right
 bool hVal_Changed = false;  
@@ -49,12 +49,12 @@ char yZero = 0;
 
 void setup() {
   // Wait before attempting to initialise sensor
-  delay(500);  
+  delay(250);  
   
   // Initialise the I2C connection
   Serial.begin(9600);
   while (!Serial) {;}
-  Serial.println("Begining Setup");
+  Serial.println("Begining Setup ... ");
   Wire.begin();
 
   SlewInit();
@@ -62,12 +62,12 @@ void setup() {
 
   // Initialise the pin mode, pullup resistors and the last known state of each button and hat direction
   for (i=0; i<uBtns; i++) {
-    pinMode(sBtn[i], INPUT);
+    pinMode(sBtn[i], INPUT_PULLUP);
     digitalWrite(sBtn[i], HIGH);
     sBtn_LastState[i] = 0;
   }
   for (i=0; i< hBtns; i++) {
-    pinMode(sHat[i], INPUT);
+    pinMode(sHat[i], INPUT_PULLUP);
     digitalWrite(sHat[i], HIGH);
     sHat_LastState[i] = 0;
   }
@@ -156,28 +156,28 @@ void loop() {
 }
 
 void SlewInit() {
-  delay(100);
   #ifdef DEBUGTHQ 
-    Serial.print("Resetting ...");
+    Serial.print("Resetting ... ");
   #endif
 
+  // Initiate Soft Reset
   WriteI2CReg(SLEW_ADR, CTR_REG1, 0x02);
-  
-  WriteI2CReg(SLEW_ADR, CTR_REG1, 0);
+  WriteI2CReg(SLEW_ADR, CTR_REG1);
 
-  char Reset_Status = 0;
-  //while (Reset_Status != 0xF0) {
+  byte Reset_Status = 0;
+  while (Reset_Status != 0xF1) {
     Reset_Status = ReadI2CReg(SLEW_ADR, CTR_REG1);
-    Serial.print(" ");
-    Serial.print(Reset_Status, HEX);
-  //}
+  }
+  Serial.print("Got code 0x");
+  Serial.print(Reset_Status, HEX);
   Serial.println(" Done");
 
 
-  WriteI2CReg(SLEW_ADR, CTR_REGT, 0x06);
+  //WriteI2CReg(SLEW_ADR, CTR_REGT, 0x06);
 
   char X, Y;                
-  char xTemp=0, yTemp=0, temp;
+  char xTemp=0, yTemp=0;
+  byte temp;
   ReadXAxis();  
   ReadYAxis(); 
   delay(5);
@@ -189,12 +189,19 @@ void SlewInit() {
   }
   xZero = xTemp/4;
   yZero = yTemp/4;
-  temp = ReadI2CReg(SLEW_ADR, ID_CODE);
 
-  if ( temp != 0x0C) {
-    Serial.print("Slew sensor failed to respond. got value: ");
-    Serial.println(temp, HEX);
-  }
+  Serial.print("xZero = ");
+  Serial.println(xZero, DEC);
+
+  Serial.print("yZero = ");
+  Serial.println(yZero, DEC);
+
+  //temp = ReadI2CReg(SLEW_ADR, ID_CODE);
+
+  //if ( temp != 0x0C) {
+  //  Serial.print("Slew sensor failed to respond. got value: ");
+  //  Serial.println(temp, HEX);
+  //}
 }
 
 void SlewSetup(char xNull, char yNull, byte xDelta, byte yDelta) {
@@ -208,6 +215,18 @@ void SlewSetup(char xNull, char yNull, byte xDelta, byte yDelta) {
   WriteI2CReg(SLEW_ADR, YP_REG, yp);
   WriteI2CReg(SLEW_ADR, YN_REG, yn);
 
+  Serial.print("XPos = ");
+  Serial.println(xp ,DEC);
+
+  Serial.print("XNeg = ");
+  Serial.println(xn ,DEC);
+
+  Serial.print("YPos = ");
+  Serial.println(yp ,DEC);
+
+  Serial.print("YNeg = ");
+  Serial.println(yn ,DEC);
+
   byte temp = ReadI2CReg(SLEW_ADR, CTR_REG1);
   temp = temp & 0b11110111;
   temp = temp & 0b01111111;
@@ -215,6 +234,11 @@ void SlewSetup(char xNull, char yNull, byte xDelta, byte yDelta) {
   temp = temp | 0b01010000;
   temp = temp | 0b00000100;
   WriteI2CReg(SLEW_ADR, CTR_REG1, temp);
+
+  WriteI2CReg(SLEW_ADR, CTR_REG2, 0x86);
+  WriteI2CReg(SLEW_ADR, CTR_REG1, 0x80);
+
+  Serial.println("Setup Done.");
 }
 
 char ReadI2CReg(byte slave_adr, byte reg) {
@@ -223,7 +247,7 @@ char ReadI2CReg(byte slave_adr, byte reg) {
   Wire.beginTransmission(slave_adr);
   Wire.write(reg);
   Wire.endTransmission();
-  Wire.requestFrom(slave_adr, (byte)1, 1);
+  Wire.requestFrom(slave_adr, 1, 1);
   temp = Wire.read();
   /* Serial.print("Communicating with device ");
   Serial.print(slave_adr, HEX);
@@ -234,20 +258,29 @@ char ReadI2CReg(byte slave_adr, byte reg) {
   return temp;
 }
 
+void WriteI2CReg(byte slave_adr, byte reg) {
+ byte error;
+ 
+  Wire.beginTransmission(slave_adr);
+  Wire.write(reg);
+  error = Wire.endTransmission();
+  if (error) {
+    Serial.print("WriteI2C :: Error code: ");
+    Serial.println(error, HEX);
+  }
+}
+
 void WriteI2CReg(byte slave_adr, byte reg, byte buf) {
   byte error;
-  /* Serial.print("Communicating with device ");
-  Serial.print(slave_adr, HEX);
-  Serial.print(", writing ");
-  Serial.print(buf, HEX);
-  Serial.print(" to register ");
-  Serial.println(reg, HEX); */
+ 
   Wire.beginTransmission(slave_adr);
   Wire.write(reg);
   Wire.write(buf);
   error = Wire.endTransmission();
-  Serial.print("WriteI2C :: Error code: ");
-  Serial.println(error, HEX);
+  if (error) {
+    Serial.print("WriteI2C :: Error code: ");
+    Serial.println(error, HEX);
+  }
 }
 
 char ReadXAxis(){
